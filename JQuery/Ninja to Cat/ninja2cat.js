b92ac2e2d28b0ac096d57533d794a63d8143c12b
Taylor Fancher <tfanch24@gmail.com>
2021-05-26 18:19:28 -0500

    first commit

---
+++ JQuery/Ninja to Cat/ninja2cat.js
@@ -0,0 +1,17 @@
+$('document').ready(function(){
+    $('#1').click(function(){
+        $(this).attr('src', 'ninja-cat/img/cat0.png')
+    })
+    $('#2').click(function(){
+        $(this).attr('src', 'ninja-cat/img/cat1.png')
+    })
+    $('#3').click(function(){
+        $(this).attr('src', 'ninja-cat/img/cat2.png')
+    })
+    $('#4').click(function(){
+        $(this).attr('src', 'ninja-cat/img/cat3.png')
+    })
+    $('#5').click(function(){
+        $(this).attr('src', 'ninja-cat/img/cat4.png')
+    })
+})
